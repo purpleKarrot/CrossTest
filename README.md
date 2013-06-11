b582b708d0d4547e6cffa99cb6f95bca26f18f91
Daniel Pfeifer <daniel@pfeifer-mail.de>
2013-06-11 13:00:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+CrossTest
+=========
+
+Minimal example project that:
+
+*   Uses [CMake](http://cmake.org/)
+*   Uses [Boost](http://boost.org/) (header only)
+*   Runs tests with [Wine](http://www.winehq.org/) when crosscompiled on Linux for Windows
+*   [![Build Status](https://travis-ci.org/purpleKarrot/CrossTest.png)](https://travis-ci.org/purpleKarrot/CrossTest)
---
+++ replace.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2013 Daniel Pfeifer <daniel@pfeifer-mail.de>
+ *
+ * Distributed under the Boost Software License, Version 1.0.
+ * See accompanying file LICENSE_1_0.txt or copy at
+ *	 http://www.boost.org/LICENSE_1_0.txt
+ */
+
+#include <string>
+#include <boost/algorithm/string/replace.hpp>
+#include <boost/detail/lightweight_test.hpp>
+
+int replace(int argc, char* argv[])
+{
+    std::string hello = "Hello, World!";
+
+	BOOST_TEST_EQ(boost::replace_all_copy(hello, "World", "Test"), "Hello, Test!");
+
+	return boost::report_errors();
+}
